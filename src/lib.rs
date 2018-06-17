930a47d7e8fc1935689da753dc03f679a4d923c3
Felix Yan <felixonmars@archlinux.org>
2018-06-17 11:40:08 +0800

    Fix a typo in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 //! Crossterm provides the same core functionalities for both windows and unix systems.
 //! Crossterm aims to be simple and easy to call in code.
 //! True the simplicity of Crossterm you do not have to worry about the platform your working with.
-//! You can just call the action you want to preform and under water it will check what to do based on the current platform.
+//! You can just call the action you want to perform and under water it will check what to do based on the current platform.
 
 #[macro_use]
 mod shared;
