ad8b75f448e582e9a429a2063a7dd3c1c3a58e5e
Timon <timonpost@hotmail.nl>
2018-11-14 15:13:58 +0100

    Fixed bug flush_buf(). (#42)
    * Fixed bug flush_buf() issue 42.

--- examples/examples.rs
+++ examples/examples.rs
@@ -15,37 +15,6 @@ mod cursor;
 mod some_types;
 mod input;
 
-use std::io::Write;
-
-use crossterm::style::{style, Color, DisplayableObject};
-use crossterm::terminal::terminal;
-use crossterm::Screen;
-
-use crossterm::output::TerminalOutput;
-use crossterm::cursor::TerminalCursor;
-
-use crossterm::terminal::Terminal;
-use std::{thread,time};
-
 fn main()
 {
-    let screen = Screen::new(false);
-    let terminal = Terminal::new(&screen.stdout);
-
-    // get terminal size
-    let (x, y) = terminal.terminal_size();
-
-    // set size to 30, 50
-    terminal.set_size(30,50);
-
-    // if we uncomment the line below the code will work perfectly fine and we will get the new dimensions.
-    // if we comment this line the terminal dimensions gotten from terminal_size() are equal to the old dimensions.
-
-    // thread::sleep(time::Duration::from_millis(20));
-
-    // get new dimensions
-    let (x_new, y_new) = terminal.terminal_size();
-
-    println!("old width: {} old height: {}", x, y);
-    println!("new width: {} new height: {}", x_new, y_new);
 }
