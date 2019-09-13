fd379f62c5ab2e8386ae16821ea9b604345c9759
Robert Vojta <rvojta@me.com>
2019-09-13 16:06:12 +0200

    Fix examples
    Signed-off-by: Robert Vojta <rvojta@me.com>

--- crossterm_screen/examples/raw_mode.rs
+++ crossterm_screen/examples/raw_mode.rs
@@ -1,11 +1,9 @@
-extern crate crossterm_screen;
+use std::io::stdout;
 
 use crossterm_screen::{IntoRawMode, RawScreen};
 
-use std::io::{stdout, Write};
-use std::{thread, time};
-
-pub fn raw_modes() {
+#[allow(unused_variables)]
+fn main() {
     // create a Screen instance that operates on the default output: io::stdout(). By passing in 'true', we make this screen 'raw'
     let screen = RawScreen::into_raw_mode();
     let screen = stdout().into_raw_mode();
