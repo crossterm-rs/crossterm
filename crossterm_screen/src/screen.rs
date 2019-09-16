f7184f4c05ebfd108b87710c9470cfd7cd397292
Robert Vojta <rvojta@me.com>
2019-09-16 08:32:35 +0200

    crossterm_screen -> 2018 edition
    Signed-off-by: Robert Vojta <rvojta@me.com>

--- crossterm_screen/src/screen.rs
+++ crossterm_screen/src/screen.rs
@@ -1,8 +1,8 @@
 //! A module which provides some functionalities to work with the terminal screen.
 //! Like allowing you to switch between main and alternate screen or putting the terminal into raw mode.
 
-mod alternate;
-mod raw;
-
 pub use self::alternate::AlternateScreen;
 pub use self::raw::{IntoRawMode, RawScreen};
+
+mod alternate;
+mod raw;
