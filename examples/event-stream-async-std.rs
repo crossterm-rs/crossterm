053ddb42d6a74bb9a62bef6f7d1a4f9d783be6b0
Blaž Hrastnik <blaz@mxxn.io>
2020-09-08 14:21:57 +0900

    Rely on futures-core instead of futures-util to speed up compile.
    The library itself only uses a few basic future traits.

--- examples/event-stream-async-std.rs
+++ examples/event-stream-async-std.rs
@@ -7,8 +7,8 @@ use std::{
     time::Duration,
 };
 
+use futures::{future::FutureExt, select, StreamExt};
 use futures_timer::Delay;
-use futures_util::{future::FutureExt, select, StreamExt};
 
 use crossterm::{
     cursor::position,
