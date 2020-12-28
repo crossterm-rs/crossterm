a6502b199835b7420f2847547c74969f4dc111ab
Timon Post <timonpost@hotmail.nl>
2020-12-28 09:32:55 +0100

    Fix wrong resize size return.

--- examples/event-read.rs
+++ examples/event-read.rs
@@ -11,6 +11,8 @@ use crossterm::{
     terminal::{disable_raw_mode, enable_raw_mode},
     Result,
 };
+use crossterm::event::poll;
+use std::time::Duration;
 
 const HELP: &str = r#"Blocking read()
  - Keyboard, mouse and terminal resize events enabled
@@ -29,12 +31,38 @@ fn print_events() -> Result<()> {
             println!("Cursor position: {:?}\r", position());
         }
 
+        if let Event::Resize(_, _)  = event {
+            let (original_size, new_size) = flush_resize_events(event);
+            println!("Resize from: {:?}, to: {:?}", original_size, new_size);
+        }
+
         if event == Event::Key(KeyCode::Esc.into()) {
             break;
         }
     }
 
     Ok(())
+}
+
+// Resize events can occur in batches.
+// With a simple loop they can be flushed.
+// This function will keep the first and last resize event.
+fn flush_resize_events(event: Event) -> ((u16, u16), (u16, u16)) {
+    if let Event::Resize(x, y) = event {
+        let mut last_resize = (x, y);
+        loop{
+            if let Ok(true) = poll(Duration::from_millis(50)) {
+                if let Ok(Event::Resize(x, y)) = read() {
+                    last_resize = (x, y);
+                }
+            }else {
+                break;
+            }
+        }
+
+        return ((x, y), last_resize);
+    }
+    ((0,0), (0,0))
 }
 
 fn main() -> Result<()> {
