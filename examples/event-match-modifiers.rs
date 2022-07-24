60e51be726f00b8df9c79b72a55d6b9f2a40a507
Jesse Weaver <pianohacker@gmail.com>
2022-07-24 06:06:46 -0600

    Increase support for kitty enhanced keyboard protocol (#688)

--- examples/event-match-modifiers.rs
+++ examples/event-match-modifiers.rs
@@ -2,7 +2,7 @@
 //!
 //! cargo run --example event-match-modifiers
 
-use crossterm::event::{Event, KeyCode, KeyEvent, KeyModifiers};
+use crossterm::event::{Event, KeyCode, KeyEvent, KeyEventKind, KeyModifiers};
 
 fn match_event(read_event: Event) {
     match read_event {
@@ -10,24 +10,29 @@ fn match_event(read_event: Event) {
         Event::Key(KeyEvent {
             modifiers: KeyModifiers::CONTROL,
             code,
+            ..
         }) => {
             println!("Control + {:?}", code);
         }
         Event::Key(KeyEvent {
             modifiers: KeyModifiers::SHIFT,
             code,
+            ..
         }) => {
             println!("Shift + {:?}", code);
         }
         Event::Key(KeyEvent {
             modifiers: KeyModifiers::ALT,
             code,
+            ..
         }) => {
             println!("Alt + {:?}", code);
         }
 
         // Match on multiple modifiers:
-        Event::Key(KeyEvent { code, modifiers }) => {
+        Event::Key(KeyEvent {
+            code, modifiers, ..
+        }) => {
             if modifiers == (KeyModifiers::ALT | KeyModifiers::SHIFT) {
                 println!("Alt + Shift {:?}", code);
             } else {
@@ -43,21 +48,26 @@ fn main() {
     match_event(Event::Key(KeyEvent {
         modifiers: KeyModifiers::CONTROL,
         code: KeyCode::Char('z'),
+        kind: KeyEventKind::Press,
     }));
     match_event(Event::Key(KeyEvent {
         modifiers: KeyModifiers::SHIFT,
         code: KeyCode::Left,
+        kind: KeyEventKind::Press,
     }));
     match_event(Event::Key(KeyEvent {
         modifiers: KeyModifiers::ALT,
         code: KeyCode::Delete,
+        kind: KeyEventKind::Press,
     }));
     match_event(Event::Key(KeyEvent {
         modifiers: KeyModifiers::ALT | KeyModifiers::SHIFT,
         code: KeyCode::Right,
+        kind: KeyEventKind::Press,
     }));
     match_event(Event::Key(KeyEvent {
         modifiers: KeyModifiers::ALT | KeyModifiers::CONTROL,
         code: KeyCode::Home,
+        kind: KeyEventKind::Press,
     }));
 }
