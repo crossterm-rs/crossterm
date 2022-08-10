1fee5ff30c47116f81cb822626e8388d554c1cc3
Charlie Groves <c@sevorg.org>
2022-08-10 07:16:56 +0000

    Add bracketed paste parsing (#693)

--- examples/event-read.rs
+++ examples/event-read.rs
@@ -10,8 +10,8 @@ use crossterm::event::{
 use crossterm::{
     cursor::position,
     event::{
-        read, DisableFocusChange, DisableMouseCapture, EnableFocusChange, EnableMouseCapture,
-        Event, KeyCode,
+        read, DisableBracketedPaste, DisableFocusChange, DisableMouseCapture, EnableBracketedPaste,
+        EnableFocusChange, EnableMouseCapture, Event, KeyCode,
     },
     execute,
     terminal::{disable_raw_mode, enable_raw_mode},
@@ -36,9 +36,9 @@ fn print_events() -> Result<()> {
             println!("Cursor position: {:?}\r", position());
         }
 
-        if let Event::Resize(_, _) = event {
-            let (original_size, new_size) = flush_resize_events(event);
-            println!("Resize from: {:?}, to: {:?}", original_size, new_size);
+        if let Event::Resize(x, y) = event {
+            let (original_size, new_size) = flush_resize_events((x, y));
+            println!("Resize from: {:?}, to: {:?}\r", original_size, new_size);
         }
 
         if event == Event::Key(KeyCode::Esc.into()) {
@@ -52,18 +52,15 @@ fn print_events() -> Result<()> {
 // Resize events can occur in batches.
 // With a simple loop they can be flushed.
 // This function will keep the first and last resize event.
-fn flush_resize_events(event: Event) -> ((u16, u16), (u16, u16)) {
-    if let Event::Resize(x, y) = event {
-        let mut last_resize = (x, y);
-        while let Ok(true) = poll(Duration::from_millis(50)) {
-            if let Ok(Event::Resize(x, y)) = read() {
-                last_resize = (x, y);
-            }
+fn flush_resize_events(first_resize: (u16, u16)) -> ((u16, u16), (u16, u16)) {
+    let mut last_resize = first_resize;
+    while let Ok(true) = poll(Duration::from_millis(50)) {
+        if let Ok(Event::Resize(x, y)) = read() {
+            last_resize = (x, y);
         }
-
-        return ((x, y), last_resize);
     }
-    ((0, 0), (0, 0))
+
+    return (first_resize, last_resize);
 }
 
 fn main() -> Result<()> {
@@ -74,8 +71,9 @@ fn main() -> Result<()> {
     let mut stdout = stdout();
     execute!(
         stdout,
+        EnableBracketedPaste,
         EnableFocusChange,
-        EnableMouseCapture,
+        EnableMouseCapture
         PushKeyboardEnhancementFlags(
             KeyboardEnhancementFlags::DISAMBIGUATE_ESCAPE_CODES
                 | KeyboardEnhancementFlags::REPORT_ALL_KEYS_AS_ESCAPE_CODES
@@ -89,6 +87,7 @@ fn main() -> Result<()> {
 
     execute!(
         stdout,
+        DisableBracketedPaste,
         PopKeyboardEnhancementFlags,
         DisableFocusChange,
         DisableMouseCapture
