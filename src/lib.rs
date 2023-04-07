e60e881e1de0703aea46fb4134de1539e3769650
Timon <timonpost@hotmail.nl>
2023-04-07 17:38:11 +0200

    fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #![deny(unused_imports, unused_must_use)]
 
-//! # Cross-platform Terminal Manipulation Library 
-//! 
+//! # Cross-platform Terminal Manipulation Library
+//!
 //! Crossterm is a pure-rust, terminal manipulation library that makes it possible to write cross-platform text-based interfaces (see [features](#features)). It supports all UNIX and Windows terminals down to Windows 7 (not all terminals are tested,
 //! see [Tested Terminals](#tested-terminals) for more info).
 //!
