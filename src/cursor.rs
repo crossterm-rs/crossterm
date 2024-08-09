10c2b9ed488404c7fa04c9fe708989ceb95e87c6
linrongbin16 <linrongbin16@outlook.com>
2024-08-09 13:30:40 +0800

    Update cursor.rs

--- src/cursor.rs
+++ src/cursor.rs
@@ -433,7 +433,7 @@ mod tests {
 
     use super::{
         sys::position, MoveDown, MoveLeft, MoveRight, MoveTo, MoveUp, RestorePosition,
-        SavePosition, SetCursorStyle,
+        SavePosition,
     };
 
     // Test is disabled, because it's failing on Travis
