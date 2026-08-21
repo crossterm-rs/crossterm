cdc30a9cd7f89d97a847c182feab66147eb50223
Bishops-exe <148124024+Bishops-exe@users.noreply.github.com>
2026-08-21 02:06:18 +0200

    feat: add `MouseEventKind::get_button()` (#1111)
    * feat: add `MouseEventKind::get_button()`
    
    * style: fix formatting
    
    Co-authored-by: Orhun Parmaksız <orhunparmaksiz@gmail.com>
    
    * docs: Document `get_button` method for `MouseEventKind`
    
    * Rename get_button to button in MouseEventKind
    
    ---------
    
    Co-authored-by: Bishops-exe <Bishops-exe@users.noreply.github.com>
    Co-authored-by: Orhun Parmaksız <orhunparmaksiz@gmail.com>

--- src/event.rs
+++ src/event.rs
@@ -799,6 +799,21 @@ pub enum MouseEventKind {
     ScrollRight,
 }
 
+impl MouseEventKind {
+    /// Returns the mouse button connected with the event.
+    ///
+    /// This will return `Some(MouseButton)` for `Down`, `Up`, and `Drag` events.
+    /// For other events, like `Moved` or `Scroll...`, it returns `None`.
+    pub fn button(&self) -> Option<MouseButton> {
+        match self {
+            MouseEventKind::Down(button)
+            | MouseEventKind::Up(button)
+            | MouseEventKind::Drag(button) => Some(*button),
+            _ => None,
+        }
+    }
+}
+
 /// Represents a mouse button.
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 #[cfg_attr(feature = "derive-more", derive(IsVariant))]
@@ -1707,5 +1722,20 @@ mod tests {
             assert_eq!(event.as_paste_event(), Some(""));
             assert_eq!(event.as_key_event(), None);
         }
+    }
+
+    #[test]
+    fn event_button() {
+        let event = MouseEventKind::Down(MouseButton::Left);
+        assert_eq!(event.button(), Some(MouseButton::Left));
+
+        let event = MouseEventKind::Up(MouseButton::Right);
+        assert_eq!(event.button(), Some(MouseButton::Right));
+
+        let event = MouseEventKind::Drag(MouseButton::Middle);
+        assert_eq!(event.button(), Some(MouseButton::Middle));
+
+        let event = MouseEventKind::Moved;
+        assert_eq!(event.button(), None);
     }
 }
